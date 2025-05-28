eba880ccce2c4bc24811385105d1dae8d97a2062
Yash <yash.vyas@dngsoftwares.com>
2025-05-28 17:35:21 +0530

    Applied stashed changes to Yash branch and resolved merge conflict in index.js

--- routes/salesRFQApprovalRoutes.js
+++ routes/salesRFQApprovalRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const SalesRFQApprovalController = require('../controllers/salesRFQApprovalController');
+// const SalesRFQApprovalController = require('../controllers/salesRFQApprovalController');
 
-router.post('/', SalesRFQApprovalController.createSalesRFQApproval);
-router.put('/', SalesRFQApprovalController.updateSalesRFQApproval);
-router.delete('/', SalesRFQApprovalController.deleteSalesRFQApproval);
-router.get('/:salesRFQId/:approverId', SalesRFQApprovalController.getSalesRFQApproval);
-router.get('/', SalesRFQApprovalController.getAllSalesRFQApprovals);
-router.get('/paginated', SalesRFQApprovalController.getPaginatedSalesRFQApprovals);
+// router.post('/', SalesRFQApprovalController.createSalesRFQApproval);
+// router.put('/', SalesRFQApprovalController.updateSalesRFQApproval);
+// router.delete('/', SalesRFQApprovalController.deleteSalesRFQApproval);
+// router.get('/:salesRFQId/:approverId', SalesRFQApprovalController.getSalesRFQApproval);
+// router.get('/', SalesRFQApprovalController.getAllSalesRFQApprovals);
+// router.get('/paginated', SalesRFQApprovalController.getPaginatedSalesRFQApprovals);
 
 module.exports = router;
