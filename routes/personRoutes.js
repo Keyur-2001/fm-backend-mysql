8c6417ad96e5ccc9cdee2545e69caa8a917ee033
Keyur-2001 <keyur.it2001@gmail.com>
2025-05-30 15:11:25 +0530

    Now Person can add and update ProfileImage

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const PersonController = require('../controllers/personController');
+const authMiddleware = require('../middleware/authMiddleware');
+const upload = require('../middleware/upload');
 
 router.get('/', PersonController.getAllPersons);
-router.post('/', PersonController.createPerson);
+router.post('/',authMiddleware, PersonController.createPerson);
 router.get('/:id', PersonController.getPersonById);
-router.put('/:id', PersonController.updatePerson);
-router.delete('/:id', PersonController.deletePerson);
+router.put('/:id', authMiddleware, PersonController.updatePerson);
+router.delete('/:id', authMiddleware, PersonController.deletePerson);
+router.post('/:id/upload-image', authMiddleware, upload.single('image'), PersonController.uploadProfileImage);
 
 module.exports = router;
