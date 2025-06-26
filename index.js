6168a56174810a551f7e35a49fec20c2eb6472b5
dev kavar <kavardev1552004@gmail.com>
2025-06-26 14:53:56 +0530

    pagination and total records updated

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ async function startServer() {
     });
 
     // dummy
-    const PORT = process.env.PORT || 7001;
+    const PORT = process.env.PORT || 7000;
     server.listen(PORT, () => {
       console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
     });
