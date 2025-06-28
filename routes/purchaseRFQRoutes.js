4322c719b6b5fa413ffe51b7c9ff41be1eb4142f
Keyur-2001 <keyur.it2001@gmail.com>
2025-06-28 17:00:23 +0530

    PurchaseRFQ Route File Updated

--- routes/purchaseRFQRoutes.js
+++ routes/purchaseRFQRoutes.js
@@ -6,19 +6,19 @@ const tableAccessMiddleware = require('../middleware/tableAccessMiddleware');
 const permissionMiddleware =  require('../middleware/permissionMiddleware')
 
 // Get all Purchase RFQs
-router.get('/',  PurchaseRFQController.getAllPurchaseRFQs);
+router.get('/',  tableAccessMiddleware, permissionMiddleware('read'), authMiddleware, PurchaseRFQController.getAllPurchaseRFQs);
 
 // Create a new Purchase RFQ
-router.post('/', authMiddleware, PurchaseRFQController.createPurchaseRFQ);
+router.post('/',  tableAccessMiddleware, permissionMiddleware('write'), authMiddleware, PurchaseRFQController.createPurchaseRFQ);
 
 // Get a single Purchase RFQ by ID
-router.get('/:id',  PurchaseRFQController.getPurchaseRFQById);
+router.get('/:id', tableAccessMiddleware, permissionMiddleware('read'),   PurchaseRFQController.getPurchaseRFQById);
 
 // Update a Purchase RFQ
-router.put('/:id', authMiddleware, PurchaseRFQController.updatePurchaseRFQ);
+router.put('/:id', tableAccessMiddleware, permissionMiddleware('update'),  authMiddleware, PurchaseRFQController.updatePurchaseRFQ);
 
 // Delete a Purchase RFQ
-router.delete('/:id', authMiddleware, PurchaseRFQController.deletePurchaseRFQ);
+router.delete('/:id', tableAccessMiddleware, permissionMiddleware('delete'),  authMiddleware, PurchaseRFQController.deletePurchaseRFQ);
 
 // Approve a Purchase RFQ
 router.post('/approve', authMiddleware, PurchaseRFQController.approvePurchaseRFQ);
